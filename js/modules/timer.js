8552fffd48b2486a806aa08baafec06401bbabc9
Oleksandr Sukhariev <sukhariev0212@gmail.com>
2023-01-27 11:06:37 +0200

    Create timer.js
    create timer.js

---
+++ js/modules/timer.js
@@ -0,0 +1,71 @@
+
+
+
+function timer(id, deadLine){
+
+    
+//////////////////// Timer /////////////////////////////////////////////
+
+    function gettimeRemaining(endtime){
+        let days,hours,minutes,seconds
+        const t = Date.parse(endtime) - Date.parse(new Date());
+
+        if(t<=0){
+            days =0;
+            hours =0;
+            minutes=0;
+            seconds=0;
+        }else{
+            days = Math.floor(t / (1000 * 60 * 60 * 24));
+            hours = Math.floor(t / (1000 * 60 * 60) % 24);
+            minutes = Math.floor((t /1000 / 60) % 60);
+            seconds = Math.floor((t / 1000) % 60);
+        }
+     
+
+        
+        return{
+            'total':t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds':seconds
+        }
+
+    }
+
+    function setClock(selector, endtime){
+        const timer = document.querySelector(selector);
+        const days = timer.querySelector('#days');
+        const hours = timer.querySelector('#hours');
+        const minutes = timer.querySelector('#minutes');
+        const seconds = timer.querySelector('#seconds');
+        
+        const timeInterval = setInterval(updateClock,1000)
+
+        updateClock();
+
+        function getNum(num){
+                if(num >=0 && num < 10){
+                    return `0${num}`;
+                }else{
+                    return num;
+                }
+        }
+        function updateClock(){
+            const t = gettimeRemaining(endtime);
+
+            days.innerHTML = getNum(t.days);
+            hours.innerHTML = getNum(t.hours);
+            minutes.innerHTML = getNum(t.minutes);
+            seconds.innerHTML = getNum(t.seconds);
+
+            if(t.total <= 0 ){
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+        setClock(id, deadLine)
+}
+export default timer;
